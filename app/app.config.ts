06fb8e7fb8ffa946c25c7c50a651ba5c16ce075e
風で旅士 <tsengze@gmail.com>
2026-03-07 23:32:25 +0800

    更新 app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
   coffee: 'https://sink.cool/coffee',
   twitter: 'https://sink.cool/x',
   telegram: 'https://sink.cool/telegram',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: '短链接服务',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
